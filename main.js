86b629c1334473bd1d95c6559b1cd33995750564
ApplePiezy <51101240+zappyz@users.noreply.github.com>
2023-08-16 15:50:00 +0200

    test scene + render

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
 import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const render = new THREE.WebGL1Renderer();
+render.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( render.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame( animate );
+    
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    render.render( scene, camera );
+}
+
+animate();
